43baa1c673ed147d0b9539a59367c85473705c9f
Iago Chiapetta <iagochiapetta@gmail.com>
2022-07-20 15:11:58 -0300

    Adicionadas novas páginas
    Adicionadas novas páginas e animações pela barra de navegação

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 function pag_interface() {
-    alert("Funciona!!!");
+    window.location.href = "/paginas/interface.html";
+}
+
+function pag_sistema() {
+    window.location.href = "/paginas/sistemas.html";
+}
+
+function home() {
+    window.location.href - "index.html";
+}
+
+function pag_aplicativos() {
+    window.location.href = "/paginas/aplicativos.html";
+}
+
+function about() {
+    window.location.href = "/paginas/sobre.html";
 }
